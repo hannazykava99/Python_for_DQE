31f74af45590d6f3db3e1d59f4c521ef8183a11d
Hanna Zykava <hanna.zykava@external.danone.com>
2022-02-20 21:03:02 +0300

    5th hw changes'

--- HW_module_5.py
+++ HW_module_5.py
@@ -3,104 +3,139 @@ from random import randint
 
 
 class FileInsert:
-    text = None
 
+   # create a method which formats the text according to requirements
+    def formatting(self, text):
+        text[0] = text[0] + ('-' * (40 - len(text[0])))
+        text.append(('-' * 40) + '\n')
+
+    # create a method which inserts the formatted text into text file (create this file if necessary or add new information if file exists)
     def inserting(self, text):
-        self.text = text
+        # open, populate and close the file
         with open("Text.txt", "a", encoding='utf-8') as file:
-            for line in self.text:
+            self.formatting(text)
+            for line in text:
                 file.write(line + '\n')
 
 
-class News(FileInsert):
+class CheckInput:
 
+    # create a method which checks that we insert only numerical values (if not - try again)
+    def input_int(self, text):
+        while True:
+            try:
+                val = int(input(text))
+                break
+            except:
+                print('Enter a numeric value, please')
+        print(val)
+        return val
+
+    # create a method which checks that we don't leave the input field empty (if it is empty - try again)
+    def input_string(self, text):
+        while True:
+            val = input(text)
+            if len(val) > 0:
+                break
+            else:
+                print('Enter something, please')
+        print(val)
+        return val
+
+
+
+class News(FileInsert, CheckInput):
+
+    # create a method which creates a news (text, city, current date + time)
     def publishing(self):
         data = []
-        data.append('\nNews: -----------------------------')
-        news = input('Enter the text of your news: ')
-        city = input('Enter the city: ')
+        data.append('News: ')
+        news = self.input_string('Enter the text of your news: ')
+        city = self.input_string('Enter the city: ')
         data.append(news)
         data.append(city.capitalize() + ', ' + str(datetime.now().strftime("%d/%m/%Y %H:%M")))
-        data.append('-----------------------------------')
         return data
 
 
-class Advertisement(FileInsert):
-    current_day = date.today()
-    expiration_date = date.today()
-    isTrue = True
-    data = []
+class Advertisement(FileInsert, CheckInput):
 
+    # create a method which creates an advertisement (text, expiration day, how many days left)
     def publishing(self):
-        self.data = []
-        ad = input('Enter the text of the advertisement: ')
-        while self.isTrue:
+        data = []
+        ad = self.input_string('Enter the text of the advertisement: ')
+        is_true = True
+        # set the expiration date by default as current day
+        expiration_date = date.today()
+        while is_true:
             try:
-                d = input('Enter expiration day of the advertisement in numeric format: ')
-                m = input('Enter expiration month of the advertisement in numeric format: ')
-                y = input('Enter expiration year of the advertisement in numeric format: ')
-                self.expiration_date = date(int(y), int(m), int(d))
-                self.isTrue = False
+                d = self.input_int('Enter expiration day of the advertisement in numeric format: ')
+                m = self.input_int('Enter expiration month of the advertisement in numeric format: ')
+                y = self.input_int('Enter expiration year of the advertisement in numeric format: ')
+                expiration_date = date(y, m, d)
+                is_true = False
             except ValueError:
                 print("This date doesn't exist. Try again")
             except OverflowError:
                 print("This date doesn't exist. Try again")
-        deadline = self.expiration_date
-        if self.current_day > deadline:
+        if expiration_date < date.today():
             comment = "Advertising is outdated"
-        elif self.current_day == deadline:
+        elif expiration_date == date.today():
             comment = "Today is the last day of the advertisement"
         else:
-            diff = deadline - self.current_day
+            diff = expiration_date - date.today()
             if diff.days == 1:
                 comment = str(diff.days) + ' day left'
             else:
                 comment = str(diff.days) + ' days left'
-        self.data.append('\nPrivate Ad: -----------------------')
-        self.data.append(ad)
-        last_string = 'Actual until: ' + deadline.strftime("%d/%m/%Y") + ', ' + comment
-        self.data.append(last_string)
-        self.data.append('-----------------------------------')
-        return self.data
+        data.append('Private Ad: ')
+        data.append(ad)
+        data.append('Actual until: ' + expiration_date.strftime("%d/%m/%Y") + ', ' + comment)
+        return data
 
 
-class Divination(FileInsert):
-    data = []
-    isTrue = True
+class Divination(FileInsert, CheckInput):
 
+    # create a method which creates a divination (user asks a question and gets an answer)
     def publishing(self):
-        self.data = []
-        self.data.append('\nQuestion-divination: --------------')
+        data = []
+        data.append('Question-divination: ')
         answers = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes – definitely', 'As I see it, yes',
                         'Most likely', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again',
                         'Don\'t count on it', 'My reply is no', 'My sources say no', 'Very doubtful']
-        while self.isTrue:
-            question = input('Your question is: ')
+        is_true = True
+        while is_true:
+            question = self.input_string('Your question is: ')
+            # check that user ask a question: the input should contain "?" at the end of sentence
             if question[-1] == '?':
-                self.isTrue = False
-                self.data.append(question)
+                is_true = False
+                data.append(question)
             else:
                 print('It seems you didn\'t write a question')
-        self.data.append(answers[randint(0, len(answers) - 1)])
-        self.data.append('I hope that helped!')
-        self.data.append('-----------------------------------')
-        return self.data
+        # generate a divination using random lib
+        data.append(answers[randint(0, len(answers) - 1)])
+        data.append('I hope that helped!')
+        return data
 
 
-while True:
-    print('What do you want to print?', '1 - News', '2 - Private Ad', '3 - Divination', '4 - Nothing', sep='\n')
-    reply = input('Choose the appropriate number: ')
-    if reply == '1':
-        data = News().publishing()
-        News().inserting(data)
-    elif reply == '2':
-        data = Advertisement().publishing()
-        Advertisement().inserting(data)
-    elif reply == '3':
-        data = Divination().publishing()
-        Divination().inserting(data)
-    elif reply == '4':
-        print('Stop')
-        break
-    else:
-        print('Try again')
+if __name__ == "__main__":
+    # ask a user what data he wants to print and then call a class and insert the data into file using inserting method
+    while True:
+        print('What do you want to print?', '1 - News', '2 - Private Ad', '3 - Divination', '4 - Nothing', sep='\n')
+        reply = input('Choose the appropriate number: ')
+        if reply == '1':
+            data = News().publishing()
+            News().inserting(data)
+            print('The news is published in Text.txt file\n')
+        elif reply == '2':
+            data = Advertisement().publishing()
+            Advertisement().inserting(data)
+            print('The advertisement is published in Text.txt file\n')
+        elif reply == '3':
+            data = Divination().publishing()
+            Divination().inserting(data)
+            print('The divination is published in Text.txt file\n')
+        elif reply == '4':
+            print('Stop')
+            break
+        else:
+            print('Try again')
